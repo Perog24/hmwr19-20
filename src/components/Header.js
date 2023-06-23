e6ccf6004576d2e97e97b0a806f5647a646c5d65
Perog24 <perog24@gmail.com>
2023-06-23 09:28:03 +0300

    Header try use useLocation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,35 @@
-import { navLinks } from "../constants"
+import React, { useState, useEffect } from "react";
+import { navLinks } from "../constants";
 
+import { Container, Nav, Navbar } from "react-bootstrap";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Header() {
+   const [activeIndex, setActiveIndex] = useState(0);
 
+   useEffect(() => {
+      const storedActiveIndex = localStorage.getItem("activeIndex");
+      if (storedActiveIndex) {
+        setActiveIndex(parseInt(storedActiveIndex));
+      }
+    }, []);
+
+   const setActive = (index) => {
+     setActiveIndex(index);
+     localStorage.setItem("activeIndex", index.toString());
+   };
    return (
-      <header>
-         <nav>
-            <ul>
-         {navLinks.map((link, index)=>(
-            <li><a href={link.linkTo}>{link.name}</a></li>
-         ))}
-            </ul>
-         </nav>
+      <header >   
+         <Navbar bg="dark" data-bs-theme="dark">
+            <Container>
+               <Nav className="me-auto">           
+               {navLinks.map((link, index)=>(<Nav.Link key={index} href={link.linkTo}  onClick={() => setActive(index)}
+                className={activeIndex === index ? "active" : ""}
+              >{link.name}</Nav.Link>))}
+               </Nav>
+            </Container>
+         </Navbar>
       </header>
    )
 }
